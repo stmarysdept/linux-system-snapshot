96eab68884d62a3e4d2e32cbda9da704f0ceb90e
stmarysdept <stmarysdept@proton.me>
2026-03-04 21:35:37 -0600

    Refactored print statements into functions to prevent future spaghetti code, added uptime and disk usage statistics, improved formatting for general radability :)

--- snapshot.py
+++ snapshot.py
@@ -1,14 +1,50 @@
 import datetime
 import socket
+import subprocess
 
-def main():
-    
-    print("The current user is: ", end='')
-    print(socket.gethostname())
+def get_hostname():
+    return socket.gethostname()
 
+def get_datetime():
     t = datetime.datetime.now()
-    formatted_t = t.strftime("%d/%m/%Y, and it's currently: %H:%M:%S")
-    print(f"The date is: {formatted_t}")
+    return t.strftime("%Y-%m-%d, and it's currently: %H:%M:%S")
 
+def get_uptime():
+    uptime = subprocess.run(
+            ["uptime", "-s"], 
+            capture_output=True, 
+            text=True
+        )
+    return uptime.stdout.strip()
+
+def get_uptime_xtra():
+    uptime_xtra = subprocess.run(
+            ["uptime", "-p"], 
+            capture_output=True, 
+            text=True
+        )
+    return uptime_xtra.stdout.strip()
+     # I really don't like the fact I had to make another
+     # function just to relay a more human version of uptime
+     # but I'll figure out how to fix that soon.
+
+def get_disk():
+    disk = subprocess.run(
+            ["df", "/home", "-h"],
+            capture_output=True,
+            text=True
+        )
+    return disk.stdout.strip()
+
+
+def main(): # would i rather have a thousand print commands
+            # or a thousand newlines.. hmm...
+        print("========================= CURRENT STATS ===============================")
+        print(f"HOSTNAME:\n{get_hostname()} \n\n")
+        print(f"CURRENT DATE:\n{get_datetime()} \n\n")
+        print(f"UPTIME SINCE:\n{get_uptime()}, {get_uptime_xtra()} \n\n")
+        print("DISK USAGE DATA:\n")
+        print(f"{get_disk()}")
+        print("=======================================================================")
 if __name__ == "__main__":
-    main()
+     main()
